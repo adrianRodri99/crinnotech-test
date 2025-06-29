c8955a1493328077f7a5b81baada164c685f1e57
Adrian <adrian.aguirre@prdavinci.tech>
2025-06-29 10:01:28 -0500

    Add global styles and type definitions for SVG icons
    - Created a new globals.css file to include Tailwind CSS base, components, and utilities.
    - Added index.ts file to define IconSvgProps type for SVG icon components, allowing optional size property.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,39 @@
 "use client";
 
-import { Provider } from 'react-redux';
-import { Toaster } from "sonner";
+import type { ThemeProviderProps } from "next-themes";
+
+import * as React from "react";
+import { HeroUIProvider } from "@heroui/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Toaster } from "sonner";
 import NotificationManager from "./components/NotificationManager";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
   return (
-    <Provider store={store}>
-      {children}
-      <NotificationManager />
-      <Toaster richColors position="top-right" />
-    </Provider>
+    <HeroUIProvider navigate={router.push}>
+      <Provider store={store}>
+        <NotificationManager />
+        <Toaster richColors position="top-right" />
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </Provider>
+    </HeroUIProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
-// tailwind.config.js
-import {heroui} from "@heroui/react";
+import {heroui} from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
-};
+}
 
-export default config;
+module.exports = config;
